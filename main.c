6854d86db9c1f5d0919a4d02fdc8c729dfce81f0
Serge Selenou <serge.selenou@bison.howard.edu>
2019-12-09 04:48:36 +0000

    added main

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 
 int main() {
   printf("Tests for linked list implementation\n");
-  list_t *l1 = list_alloc();
-  list_add_to_front(l1,2);
-  list_print(l1);
+  //list_t *l1 = list_alloc();
+  //list_add_to_front(l1,2);
+  //list_print(l1);
   return 0;
 }
